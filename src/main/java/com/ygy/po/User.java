bf206388a308ff2ce492f92d624c62252294e5f6
硕大的根 <122967168@qq.com>
2020-09-13 23:28:18 +0800

    init gittest project

---
+++ src/main/java/com/ygy/po/User.java
@@ -0,0 +1,8 @@
+package com.ygy.po;
+
+/**
+ * @author ygy
+ * @create 2020-09-13 23:21
+ */
+public class User {
+}
---
+++ src/test/java/com/ygy/test/UserTest.java
@@ -0,0 +1,8 @@
+package com.ygy.test;
+
+/**
+ * @author ygy
+ * @create 2020-09-13 23:22
+ */
+public class UserTest {
+}
